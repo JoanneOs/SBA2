169618fad1cc58c393d1510748a3e9010fcf7cd9
JoanneOs <jowan.awsman@yahoo.com>
2025-03-18 21:37:55 -0500

    elements

---
+++ index.js
@@ -0,0 +1,4 @@
+// first grab all the elements we need to work with
+const form = document.getElementById('registrationForm');
+const welcomeMessage = document.getElementById('welcomeMessage');
+const welcomeUser = document.getElementById('welcomeUser');
