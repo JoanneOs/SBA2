19d6bf61888de6d413062408ecc33dfc7004010a
JoanneOs <jowan.awsman@yahoo.com>
2025-03-18 21:39:22 -0500

    input

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
 const form = document.getElementById('registrationForm');
 const welcomeMessage = document.getElementById('welcomeMessage');
 const welcomeUser = document.getElementById('welcomeUser');
+
+// These are our input fields
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
